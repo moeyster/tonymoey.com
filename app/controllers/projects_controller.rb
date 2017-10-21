8297e0a86d59fb3c0fe964b4e3073a0f35de5f60
Tony Moey <moeyster@gmail.com>
2017-10-22 06:26:46 +0800

    Added projects controller and built simple resume template for the json parser.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,4 @@
+class ProjectsController < ApplicationController
+  def json_parser
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#index'
+  resources :projects do
+    collection do
+      get :json_parser
+    end
+  end
 end
